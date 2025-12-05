039a53c0dd30b766c1c3a0eb77e755f623d297fb
devMarcela <gfreitas.marcela@gmail.com>
2025-12-05 16:23:23 -0300

    refactor: simplify main file and add gitignore

--- crud.main.py
+++ crud.main.py
@@ -1,18 +1,4 @@
-# main.py
-"""
-Entry point for the Quiz CRUD Application (Project 2).
-This file imports the CRUD UI module and starts the main window.
-"""
+from crud_interface import run_crud
 
-from crud_ui import run_crud_app  # impo
-
-
-
- rt the main window function from CRUD interface
-
-def main():
-    """Start the CRUD interface window."""
-    run_crud_app()  # call the GUI function that creates and runs the Tkinter window
-
-if __name__ == "__main__":  # check if this file is executed as the main script
-    main()  # execute the main function to start the app
+if __name__ == "__main__":
+    run_crud()
